5bc8e65941ffe640041673ac5787d487d83d9aef
leey2 <leey2@WIT45001492.wit.private>
2019-09-16 20:17:15 -0400

    AMEN

--- Project 1/src/ahhh.java
+++ Project 1/src/ahhh.java
@@ -4,6 +4,7 @@ public class ahhh {
 	public static void main(String[] args) {
 		System.out.printf("Jonny Ho is a Ho%n");
 		System.out.printf("Eddie is an eddiot");
+		System.out.printf("Only thing to trust is StackOverflow (AMEN) the one true god");
 	}
 
 }
