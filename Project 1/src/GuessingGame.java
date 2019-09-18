68970b371d7861093842bb6cb92dce0d87acabf3
hoj2 <hoj2@WIT45001220.wit.private>
2019-09-18 12:17:26 -0400

    done(not really)

--- Project 1/src/GuessingGame.java
+++ Project 1/src/GuessingGame.java
@@ -3,6 +3,11 @@ import java.util.Random;
 
 public class GuessingGame {
 	
+	private static int randAmnt;
+	//Sets a maximum for what the numbers can range up to.
+	private static int Max = 10;
+	private static Random rand = new Random();
+	
 	//Generic main method to start program.
 	public static void main(String[] args) {
 		gameStart();
@@ -10,27 +15,21 @@ public class GuessingGame {
 	
 	//Actual game method
 	@SuppressWarnings({ "rawtypes", "unchecked"})
-	public static void gameStart() {
+	private static void gameStart() {
+		
 		//Declares Scanner for user input
 		Scanner in = new Scanner(System.in);
-		
-		//Sets a maximum for what the numbers can range up to.
-		int Max = 10;
-		Random rand = new Random();
-		int randAmnt;
 		
 		//Sets boolean for if the player wants to play
 		boolean doPlay = true;
 		
+		//Boolean to see if this is the users first guess or not.
+		boolean firstGuess = true;
+		
 		while(doPlay) {
+			
 			//Declares random amount of numbers the game wants the user to guess.
 			randAmnt = rand.nextInt(Max) + 1;
-			
-			//Boolean to see if the player is still wrong
-			boolean wrong = true;
-			
-			//Boolean to see if this is the users first guess or not.
-			boolean firstGuess = true;
 			
 			//Declares and sets a linked bag that holds the answer to the game.
 			LinkedBag answer = new LinkedBag();
@@ -46,49 +45,66 @@ public class GuessingGame {
 			//Asks user for their guesses.
 			System.out.printf("Enter %d integers in the range from 1 to %d. Entries may be duplicate.%n", randAmnt, Max);
 			
+			//Creates a copy linked bag of the answer.
+			LinkedBag answerCopy = answer.copy();
+			
 			//Takes user guesses. Repeats if they guess incorrectly.
-			while(wrong) {
-				
-				//Creates a copy linked bag of the answer.
-				LinkedBag answerCopy = answer.copy();
-				
-				//Takes in user's guesses and removes them from the copy answer.
-				for(int i = 0; i < randAmnt; i++) {
-					//Requires a try and catch.
-					answerCopy.remove(in.nextInt());
-				}
-				
-				//they are correct if the copied answer is empty
-				if(answerCopy.isEmpty()) {
-					wrong = false;
-					System.out.printf("You are correct! Play again? (yes,no)%n");
-					String yesNo = "";
-					
-					//Only takes yes or no for an answer.
-					while(!yesNo.toLowerCase().equals("yes") && !yesNo.toLowerCase().equals("no")) {
-						yesNo = in.nextLine();
-					}
-					
-					if(yesNo.toLowerCase().equals("no")) {
-						doPlay = false;
-						System.out.printf("Goodbye!");
-						in.close();
-					}
-					
+			while(!guess(answer, in, firstGuess, answerCopy)) {
+				System.out.printf("%d of your guesses are correct.", (randAmnt - answerCopy.getCurrentSize())); 
+				if (firstGuess) {
+					System.out.printf(" Guess again.%n");
+					firstGuess = false;
 				}else {
-					System.out.printf("%d of your guesses are correct.", (randAmnt - answerCopy.getCurrentSize())); 
-					if (firstGuess) {
-						System.out.printf(" Guess again.%n");
-						firstGuess = false;
-					}else {
-						System.out.printf("%n");
-					}
-					
+					System.out.printf("%n");
 				}
 				
+				//Resets answer copy.
+				answerCopy = answer.copy();
+				
+			}
+			
+			//Asks user if they want to try again
+			if(!tryAgain(in)) {
+				doPlay = false;
+				System.out.printf("Goodbye!");
+				in.close();
 			}
 			
 		}
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static boolean guess(LinkedBag answer, Scanner in, boolean firstGuess, LinkedBag answerCopy) {
+		
+		//Takes in user's guesses and removes them from the copy answer.
+		for(int i = 0; i < randAmnt; i++) {
+			try {
+				answerCopy.remove(in.nextInt());
+			}catch(Exception e) {
+				System.out.printf("Please enter a valid number.%n");
+				in.nextLine();
+				i--;
+			}
+		}
+		
+		//they are correct if the copied answer is empty
+		return answerCopy.isEmpty();
+	}
+	
+	private static boolean tryAgain(Scanner in) {
+		System.out.printf("You are correct! Play again? (yes, no)%n");
+		String yesNo = "";
+		
+		//Only takes yes or no for an answer.
+		while(!yesNo.toLowerCase().equals("yes") && !yesNo.toLowerCase().equals("no")) {
+			yesNo = in.nextLine();
+		}
+		
+		if(yesNo.toLowerCase().equals("no")) {
+			return false;
+		}
+		
+		return true;
+	}
+	
 }
