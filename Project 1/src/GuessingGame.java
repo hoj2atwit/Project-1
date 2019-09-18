afba6abafdeb5a2725ed8ae7771449361c9cfcba
hoj2 <hoj2@WIT45001220.wit.private>
2019-09-18 09:39:45 -0400

    bah interface

--- Project 1/src/GuessingGame.java
+++ Project 1/src/GuessingGame.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 
 public class GuessingGame {
 	
 	public static void main(String[] args) {
-		System.out.printf("Jonny Ho is a Ho%n");
-		System.out.printf("Eddie is an eddiot");
-		System.out.printf("Only thing to trust is StackOverflow (AMEN) the one true god");
+		Scanner in = new Scanner(System.in);
+		
+		
 	}
 
 }
